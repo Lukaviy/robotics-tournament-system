21808e529055c63c814d7cfab0f81033e0a6773f
Mikhail Babich <babich-mv@yandex.ru>
2019-05-19 17:24:59 +1000

    Initial commit

---
+++ app/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\ResetsPasswords;
+
+class ResetPasswordController extends Controller
+{
+    use ResetsPasswords;
+
+    protected $redirectTo = '/home';
+
+    public function __construct()
+    {
+        $this->middleware('guest');
+    }
+}
